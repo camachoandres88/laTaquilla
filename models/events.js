47f5ef84492225bc535a5f92811349928c0c3b42
Jose Andres Camacho Pimentel <JCamacho@IQNetwork.hdq>
2016-02-26 19:00:07 -0500

    New Models, country and city

--- models/events.js
+++ models/events.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
 
+var countrySchema = new Schema({
+    name: String,
+    population: String
+});
+
+var citySchema = new Schema({
+    name: String,
+    country: { type: Schema.ObjectId, ref: 'Country'}
+});
+
 var categorySchema = new mongoose.Schema({
     name: {
         type: String,
@@ -17,14 +27,35 @@ var eventSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    header_image : {
+        type: String
+    },
+    background_color: {
+        type: String
+    },
+    acent_color: {
+        type: String
+    },
+    location : {type: [Number], index: '2d'},
     category : { type: mongoose.Schema.ObjectId, ref: 'Category' },
     owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
-    value :{
+    value :[{
+        tittle : {type:String},
+        description : {type:String},
         value_without_tax: { type : Number, required: true},
-        tiket_service: { type : Number, required: true}
+        tiket_service: { type : Number, required: true},
+        quantity : {type: Number}
+    }],
+    keywords : [{type: String}],
+    country :  { type: mongoose.Schema.ObjectId, ref: 'Country' },
+    city :   { type: mongoose.Schema.ObjectId, ref: 'City' },
+    address = {
+        type: String
     },
-    keywords : [{type: String}]
+    date: { type: Date, default: Date.now },
 });
 
+mongoose.model('Country',countrySchema);
+mongoose.model('City',citySchema);
 mongoose.model('Category',categorySchema);
 mongoose.model('Event',eventSchema);
